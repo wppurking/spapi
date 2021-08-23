ddecf471ced28bc96dd20df5a3eeee27afd5970e
wyatt <wppurking@gmail.com>
2021-08-23 11:09:41 +0800

    * 使用 amz 的 tempalte * 修复 gradle 相关配置以及依赖 * 生成 sourcecode

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 ## License
 
 This project is licensed under the Apache-2.0 License.
+
+
+## 使用说明
+因为需要为每一个 api 的描述文件生成 api, 而且 amazon 的 api 很多, 所以使用 ruby 脚本进行处理, 其中步骤有:
+1. 设定好 groupId, artifactId, template, invokerPackage 等生成主体的 api 代码
+2. 复制需要的 SellingPartnerAPIAA 与 Documents API
+3. 调整 gradle 的信息, build.gradle 与依赖
+
+
+使用:
+* ruby generate.rb
+* 将代码 cp 到对应目录, 例如: cp -r codegen/* /Users/wyatt/Programer/gitlab/amazon-sp-api/
+* 需要将  messaging/Schema 与 solicitations/Schema 中的 @Schema 与 Schema 区分开. 手动修改 @Schema 变为 @io.swagger.v3.oas.annotations.media.Schema
+* 最后在 <xxx>/amazon-sp-api/ 生成 jar 包, ./gradlew jar
--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ output_folder = "./codegen"
 # 两份 api 描述, 只能使用一份
 skip_files = %w[catalogItemsV0 feeds_2020-09-04 reports_2021-06-30 vendor]
 
-# 0. 清理老文件
+# - 清理老文件
 cmd0 = "rm -rf #{output_folder}"
 puts cmd0
 `#{cmd0}`
@@ -14,41 +14,47 @@ files.each do |f|
   next if skip_files.select { |sk| f.include?(sk) }.size > 0
   # 1. 生成代码
   package_name =  File.basename(f).gsub('.json', '').gsub('V0', '').split("_").first.downcase
+  template_path = 'clients/sellingpartner-api-aa-java/resources/swagger-codegen/templates'
   puts "file: #{f}, package_name: #{package_name}"
   # 使用 swagger-codegen 命令
   #command = "swagger-codegen generate --group-id com.amazon --artifact-id spapi --artifact-version 1.0.0 --invoker-package com.amazon.spapi.client --api-package com.amazon.spapi.api --model-package com.amazon.spapi.model.#{package_name} -i #{f} -l java -o #{output_folder}"
   # 直接使用 java + swagger-codegen jar 包
   # command = "java -jar swagger-codegen-cli.jar generate --group-id com.amazon --artifact-id spapi --artifact-version 1.0.0 --invoker-package com.amazon.spapi.client --api-package com.amazon.spapi.api --model-package com.amazon.spapi.model.#{package_name} -i #{f} -l java -o #{output_folder}"
-  command = "java -jar swagger-codegen-cli.jar generate --model-package com.amazon.spapi.model.#{package_name} -i #{f} -l java -c ./config.json -o #{output_folder}"
+  command = "java -jar swagger-codegen-cli.jar generate --model-package com.amazon.spapi.models.#{package_name} -t #{template_path}  -i #{f} -l java -c ./config.json -o #{output_folder}"
   puts command
   `#{command}`
 end
 
-# 2. 挪动 SellingPartnerAPIAA
-cmd2 = "cp -r clients/sellingpartner-api-aa-java/src/com #{output_folder}/src/main/java/"
-puts cmd2
-`#{cmd2}`
+# - 挪动 SellingPartnerAPIAA
+cp1 = "cp -r clients/sellingpartner-api-aa-java/src/com #{output_folder}/src/main/java/"
+puts cp1
+`#{cp1}`
 
-cmd2_1 = "cp -r clients/sellingpartner-api-aa-java/tst/com #{output_folder}/src/test/java/"
-puts cmd2_1
-`#{cmd2_1}`
+cp2 = "cp -r clients/sellingpartner-api-aa-java/tst/com #{output_folder}/src/test/java/"
+puts cp2
+`#{cp2}`
 
-# 3. 修改 gradle 版本
+# - 挪动 documents api
+cp3 = "cp -r clients/sellingpartner-api-documents-helper-java/src/main/java/com #{output_folder}/src/main/java/"
+puts cp3
+`#{cp3}`
+
+# - 修改 gradle 版本
 command3 = "cp -f gradle-wrapper.properties #{output_folder}/gradle/wrapper/"
 puts command3
 `#{command3}`
 
-# 4. 增加 gradlew 可执行权限
+# - 增加 gradlew 可执行权限
 command4 = "chmod +x #{output_folder}/gradlew"
 puts command4
 `#{command4}`
 
-# 5. 调整构建脚本
+# - 调整构建脚本
 command5 = "cp -f build.gradle #{output_folder}/"
 puts command5
 `#{command5}`
 
-# 6. 清理无用的文件
+# - 清理无用的文件
 cmd6 = "rm -rf #{output_folder}/src/main/AndroidManifest.xml"
 puts cmd6
 `#{cmd6}`
